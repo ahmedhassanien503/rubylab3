8fe980da27128a38ffec12dba46ebf137ba9af1d
ahmedhassanien503 <ahm_hassanien@yahoo.com>
2020-04-26 23:55:25 +0200

    lab3

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+  load_and_authorize_resource
+    before_action :authenticate_user!, except: [:show,:index]
+    def new      
+        @article = Article.new
+        if can? :read, @article
+        end    
+    end
+    
+    def create
+        @article=Article.new(params.require(:article).permit(:title,:text))
+        @article.id = current_user
+        if @article.save
+            redirect_to @article
+        else
+            render 'new' 
+            
+        end   
+    end  
+    
+    def show
+        @article=Article.find(params[:id]) 
+    end  
+    
+    def index
+        @articles=Article.all
+
+    end   
+
+    def edit
+        @article=Article.find(params[:id])
+    end  
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(params.require(:article).permit(:title, :text))
+          redirect_to @article
+        else
+          render 'edit'
+        end
+      end  
+      def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+     
+        redirect_to articles_path
+      end
+      
+end
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource param_method: :my_sanitizer
+  load_and_authorize_resource :through => :current_user
+
+   def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article) 
+   end
+   private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
